f4e10afa8fc548b40530e7479ab0cd4558388733
LittleSheep <littlesheep.code@hotmail.com>
2025-06-26 02:13:32 +0800

    :wastebasket: Remove snackbar testing code

--- lib/widgets/alert.dart
+++ lib/widgets/alert.dart
@@ -11,7 +11,7 @@ export 'content/alert.native.dart'
 void showSnackBar(String message, {SnackBarAction? action}) {
   showTopSnackBar(
     globalOverlay.currentState!,
-    Card(child: Text(message).padding(horizontal: 24, vertical: 16)),
+    Card(child: Text(message).padding(horizontal: 20, vertical: 16)),
     snackBarPosition: SnackBarPosition.bottom,
   );
 }
