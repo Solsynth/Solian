d0ec8d074aec8dadf9a380420696721270608b6b
LittleSheep <littlesheep.code@hotmail.com>
2025-06-28 01:28:44 +0800

    :sparkles: Android notification extension

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,6 +57,8 @@ android {
 
 dependencies {
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
 
 flutter {
