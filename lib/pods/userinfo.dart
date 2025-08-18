dd65c0637f78355610d4f6e3d60d58a056ac4ebf
LittleSheep <littlesheep.code@hotmail.com>
2025-08-18 13:08:57 +0800

    :sparkles: Userinfo failed to load alert

--- lib/pods/userinfo.dart
+++ lib/pods/userinfo.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
 import 'dart:developer';
 
+import 'package:dio/dio.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_platform_alert/flutter_platform_alert.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:island/models/account.dart';
@@ -13,6 +18,11 @@ class UserInfoNotifier extends StateNotifier<AsyncValue<SnAccount?>> {
   UserInfoNotifier(this._ref) : super(const AsyncValue.data(null));
 
   Future<void> fetchUser() async {
+    final token = _ref.watch(tokenProvider);
+    if (token == null) {
+      log('[UserInfo] No token found, not going to fetch...');
+      return;
+    }
     try {
       final client = _ref.read(apiClientProvider);
       final response = await client.get('/id/accounts/me');
@@ -20,6 +30,44 @@ class UserInfoNotifier extends StateNotifier<AsyncValue<SnAccount?>> {
       state = AsyncValue.data(user);
       FirebaseAnalytics.instance.setUserId(id: user.id);
     } catch (error, stackTrace) {
+      if (!kIsWeb) {
+        if (error is DioException) {
+          FlutterPlatformAlert.showCustomAlert(
+            windowTitle: 'failedToLoadUserInfo'.tr(),
+            text: [
+              (error.response?.statusCode == 401
+                      ? 'failedToLoadUserInfoUnauthorized'
+                      : 'failedToLoadUserInfoNetwork')
+                  .tr()
+                  .trim(),
+              error.response?.headers.toString(),
+              jsonEncode(error.response?.data),
+            ].join('\n\n'),
+            iconStyle: IconStyle.error,
+            neutralButtonTitle: 'retry'.tr(),
+            negativeButtonTitle: 'okay'.tr(),
+          ).then((value) {
+            if (value == CustomButton.neutralButton) {
+              fetchUser();
+            }
+          });
+        }
+        FlutterPlatformAlert.showCustomAlert(
+          windowTitle: 'failedToLoadUserInfo'.tr(),
+          text:
+              [
+                'failedToLoadUserInfoNetwork'.tr(),
+                error.toString(),
+              ].join('\n\n').trim(),
+          iconStyle: IconStyle.error,
+          neutralButtonTitle: 'retry'.tr(),
+          negativeButtonTitle: 'okay'.tr(),
+        ).then((value) {
+          if (value == CustomButton.neutralButton) {
+            fetchUser();
+          }
+        });
+      }
       log(
         "[UserInfo] Failed to fetch user info...",
         name: 'UserInfoNotifier',
