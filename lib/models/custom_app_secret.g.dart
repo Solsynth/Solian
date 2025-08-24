a03d6015a6a0e04bf06737c8202e0fd4545aa2e6
LittleSheep <littlesheep.code@hotmail.com>
2025-08-24 23:46:14 +0800

    :sparkles: Manage secret

--- lib/models/custom_app_secret.g.dart
+++ lib/models/custom_app_secret.g.dart
@@ -9,9 +9,11 @@ part of 'custom_app_secret.dart';
 _CustomAppSecret _$CustomAppSecretFromJson(Map<String, dynamic> json) =>
     _CustomAppSecret(
       id: json['id'] as String,
-      secret: json['secret'] as String,
+      secret: json['secret'] as String?,
       createdAt: DateTime.parse(json['created_at'] as String),
       description: json['description'] as String?,
+      expiresIn: (json['expires_in'] as num?)?.toInt(),
+      isOidc: json['is_oidc'] as bool?,
     );
 
 Map<String, dynamic> _$CustomAppSecretToJson(_CustomAppSecret instance) =>
@@ -20,4 +22,6 @@ Map<String, dynamic> _$CustomAppSecretToJson(_CustomAppSecret instance) =>
       'secret': instance.secret,
       'created_at': instance.createdAt.toIso8601String(),
       'description': instance.description,
+      'expires_in': instance.expiresIn,
+      'is_oidc': instance.isOidc,
     };
