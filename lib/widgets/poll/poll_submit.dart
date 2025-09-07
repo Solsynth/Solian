18b0dbd79704180705e5f39fee4a8443ff31c17e
LittleSheep <littlesheep.code@hotmail.com>
2025-09-07 22:20:49 +0800

    :bug: Fix post submit

--- lib/widgets/poll/poll_submit.dart
+++ lib/widgets/poll/poll_submit.dart
@@ -405,6 +405,7 @@ class _PollSubmitState extends ConsumerState<PollSubmit> {
               });
             },
             multiSelectionEnabled: false,
+            emptySelectionAllowed: true,
           ),
         ),
       ],
