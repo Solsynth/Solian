d1a50eca9a51276b01dd04f3601e4d72f26163e7
LittleSheep <littlesheep.code@hotmail.com>
2025-10-23 01:27:40 +0800

    :lipstick: Optimize post metadata

--- lib/screens/posts/post_category_detail.dart
+++ lib/screens/posts/post_category_detail.dart
@@ -237,7 +237,7 @@ class PostCategoryDetailScreen extends HookConsumerWidget {
                         loading: () => ResponseLoadingWidget(),
                       ),
                     ),
-                  ),
+                  ).padding(horizontal: 8),
                 ),
               ),
             const SliverGap(4),
