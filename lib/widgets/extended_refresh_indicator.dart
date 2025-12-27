9cc710913abfb5b68bd2c22dd86e62f24b241e18
LittleSheep <littlesheep.code@hotmail.com>
2025-12-27 22:16:00 +0800

    :recycle: Better refresh indicator

--- lib/widgets/extended_refresh_indicator.dart
+++ lib/widgets/extended_refresh_indicator.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:hotkey_manager/hotkey_manager.dart';
+import 'package:uuid/uuid.dart';
 
 class RefreshIntent extends Intent {
   const RefreshIntent();
 }
 
-class ExtendedRefreshIndicator extends StatefulWidget {
+class ExtendedRefreshIndicator extends HookConsumerWidget {
   final Widget child;
   final RefreshCallback onRefresh;
 
@@ -16,52 +23,33 @@ class ExtendedRefreshIndicator extends StatefulWidget {
   });
 
   @override
-  State<ExtendedRefreshIndicator> createState() =>
-      _ExtendedRefreshIndicatorState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final hotKeyIdentifier = useState(Uuid().v4().substring(0, 8));
 
-class _ExtendedRefreshIndicatorState extends State<ExtendedRefreshIndicator> {
-  late final FocusNode _focusNode;
-
-  @override
-  void initState() {
-    super.initState();
-    _focusNode = FocusNode();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _focusNode.requestFocus();
-    });
-  }
-
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Shortcuts(
-      shortcuts: <LogicalKeySet, Intent>{
-        LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyR):
-            const RefreshIntent(),
-        LogicalKeySet(LogicalKeyboardKey.meta, LogicalKeyboardKey.keyR):
-            const RefreshIntent(),
-        LogicalKeySet(LogicalKeyboardKey.f5): const RefreshIntent(),
-      },
-      child: Actions(
-        actions: <Type, Action<Intent>>{
-          RefreshIntent: CallbackAction<RefreshIntent>(
-            onInvoke: (RefreshIntent intent) => widget.onRefresh(),
-          ),
-        },
-        child: Focus(
-          focusNode: _focusNode,
-          child: RefreshIndicator(
-            onRefresh: widget.onRefresh,
-            child: widget.child,
-          ),
-        ),
-      ),
+    final refreshHotKey = HotKey(
+      identifier: 'refresh_indicator_$hotKeyIdentifier',
+      key: PhysicalKeyboardKey.keyR,
+      modifiers: [
+        (!kIsWeb && Platform.isMacOS)
+            ? HotKeyModifier.meta
+            : HotKeyModifier.control,
+      ],
+      scope: HotKeyScope.inapp,
     );
+
+    useEffect(() {
+      hotKeyManager.register(
+        refreshHotKey,
+        keyDownHandler: (_) {
+          onRefresh.call();
+        },
+      );
+
+      return () {
+        hotKeyManager.unregister(refreshHotKey);
+      };
+    }, []);
+
+    return RefreshIndicator(onRefresh: onRefresh, child: child);
   }
 }
