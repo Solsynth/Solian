cf3a2b63409b23ff41842194780c7bc52b2a908f
LittleSheep <littlesheep.code@hotmail.com>
2026-01-03 13:06:10 +0800

    :tada: Create the iOS extension widget target

---
+++ ios/SolianWidgetExtension/SolianWidgetExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SolianWidgetExtensionBundle.swift
+//  SolianWidgetExtension
+//
+//  Created by LittleSheep on 2026/1/3.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SolianWidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        SolianWidgetExtension()
+    }
+}
