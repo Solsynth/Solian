12e3475f8f06c5ed7c163b0d88b4d68b0f1d8374
LittleSheep <hi@littlesheep.me>
2026-03-18 01:06:48 +0800

    :sparkles: Sponsor badge

--- lib/accounts/badge.dart
+++ lib/accounts/badge.dart
@@ -17,75 +17,26 @@ class BadgeInfo {
 }
 
 const Map<String, BadgeInfo> kBadgeTemplates = {
-  'achievements.post.first': BadgeInfo(
-    type: 'achievements.post.first',
-    name: 'firstPostBadgeName',
-    description: 'firstPostBadgeDescription',
-    icon: Icons.create,
-    color: Colors.green,
-  ),
-  'achievements.post.popular': BadgeInfo(
-    type: 'achievements.post.popular',
-    name: 'popularPostBadgeName',
-    description: 'popularPostBadgeDescription',
-    icon: Icons.trending_up,
-    color: Colors.orange,
-  ),
-  'achievements.post.viral': BadgeInfo(
-    type: 'achievements.post.viral',
-    name: 'viralPostBadgeName',
-    description: 'viralPostBadgeDescription',
-    icon: Icons.whatshot,
+  'sponsor': BadgeInfo(
+    type: 'sponsor',
+    name: 'sponsorBadgeName',
+    description: 'sponsorBadgeDescription',
+    icon: Icons.favorite,
     color: Colors.red,
   ),
-  'achievements.comment.helpful': BadgeInfo(
-    type: 'achievements.comment.helpful',
-    name: 'helpfulCommentBadgeName',
-    description: 'helpfulCommentBadgeDescription',
-    icon: Icons.thumb_up,
-    color: Colors.lightBlue,
-  ),
-  'ranks.newcomer': BadgeInfo(
-    type: 'ranks.newcomer',
-    name: 'newcomerBadgeName',
-    description: 'newcomerBadgeDescription',
-    icon: Icons.person_outline,
-    color: Colors.blue,
-  ),
-  'ranks.contributor': BadgeInfo(
+  'special.contributor': BadgeInfo(
     type: 'ranks.contributor',
     name: 'contributorBadgeName',
     description: 'contributorBadgeDescription',
     icon: Icons.stars,
     color: Colors.purple,
   ),
-  'ranks.expert': BadgeInfo(
-    type: 'ranks.expert',
-    name: 'expertBadgeName',
-    description: 'expertBadgeDescription',
-    icon: Icons.workspace_premium,
-    color: Colors.amber,
-  ),
-  'event.founder': BadgeInfo(
+  'special.founder': BadgeInfo(
     type: 'event.founder',
     name: 'founderBadgeName',
     description: 'founderBadgeDescription',
     icon: Icons.foundation,
     color: Colors.deepPurple,
-  ),
-  'event.beta.tester': BadgeInfo(
-    type: 'event.beta.tester',
-    name: 'betaTesterBadgeName',
-    description: 'betaTesterBadgeDescription',
-    icon: Icons.bug_report,
-    color: Colors.teal,
-  ),
-  'special.moderator': BadgeInfo(
-    type: 'special.moderator',
-    name: 'moderatorBadgeName',
-    description: 'moderatorBadgeDescription',
-    icon: Icons.construction,
-    color: Colors.indigo,
   ),
   'special.developer': BadgeInfo(
     type: 'special.developer',
