3320a0a21157a9daa3a4e279996629192a0363e2
LittleSheep <hi@littlesheep.me>
2026-04-03 01:54:47 +0800

    :recycle: Update UDID obtain

--- lib/core/services/udid.native.dart
+++ lib/core/services/udid.native.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter_udid/flutter_udid.dart';
 
 String? _cachedUdid;
 
@@ -10,13 +11,32 @@ Future<String> getUdid() async {
     return _cachedUdid!;
   }
 
-  if (Platform.isWindows) {
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    WindowsDeviceInfo windowsInfo = await deviceInfo.windowsInfo;
-    _cachedUdid = windowsInfo.deviceId;
+  final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  String rawIdentifier;
+
+  if (Platform.isAndroid) {
+    final AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    rawIdentifier = androidInfo.id;
+  } else if (Platform.isIOS) {
+    final IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+    rawIdentifier = iosInfo.identifierForVendor ?? iosInfo.utsname.machine;
+  } else if (Platform.isMacOS) {
+    final MacOsDeviceInfo macInfo = await deviceInfo.macOsInfo;
+    rawIdentifier = macInfo.systemGUID ?? macInfo.computerName;
+  } else if (Platform.isLinux) {
+    final LinuxDeviceInfo linuxInfo = await deviceInfo.linuxInfo;
+    rawIdentifier = linuxInfo.machineId ?? linuxInfo.id;
+  } else if (Platform.isWindows) {
+    final WindowsDeviceInfo windowsInfo = await deviceInfo.windowsInfo;
+    rawIdentifier = windowsInfo.deviceId;
   } else {
-    _cachedUdid = await FlutterUdid.consistentUdid;
+    rawIdentifier = Platform.localHostname;
   }
+
+  // Generate consistent hash identifier
+  final bytes = utf8.encode(rawIdentifier);
+  final digest = sha256.convert(bytes);
+  _cachedUdid = digest.toString();
 
   return _cachedUdid!;
 }
