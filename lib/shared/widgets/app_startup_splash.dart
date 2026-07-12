d2cd6d213f61e55c0df7f1ee9517ba617644eca6
LittleSheep <hi@littlesheep.me>
2026-07-12 14:36:18 +0800

    :bug: Fix audio session configure issue, close #370

--- lib/shared/widgets/app_startup_splash.dart
+++ lib/shared/widgets/app_startup_splash.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -131,9 +133,8 @@ class StartupSplashScreen extends HookConsumerWidget {
           label: 'startupStageAudioAssets'.tr(),
           isCritical: false,
           action: () async {
+            if (kIsWeb || (!Platform.isIOS && !Platform.isMacOS)) return;
             await ref.read(audioSessionProvider.future);
-            await ref.read(notificationSfxProvider.future);
-            await ref.read(messageSfxProvider.future);
           },
         ),
         _BootstrapStage(
