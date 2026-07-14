9738d2bc2e3263c1e409a69511b98fa8c86e549f
Texas0295 <kimura@texas0295.top>
2026-07-14 15:35:51 +0800

    [BLD] appimage: compile quickjs_c_bridge from source during AppImage build
    Signed-off-by: Texas0295 <kimura@texas0295.top>

--- buildtools/build-appimage.sh
+++ buildtools/build-appimage.sh
@@ -1,6 +1,10 @@
 rm -rf Solian.AppDir
 mkdir Solian.AppDir
-cp linux/flutter/ephemeral/.plugin_symlinks/flutter_js/linux/shared/libquickjs_c_bridge_plugin.so build/linux/x64/release/bundle/lib/
+
+cmake -B build_qjs -S third_party/quickjs_c_bridge/linux/ -DCMAKE_BUILD_TYPE=Release
+cmake --build build_qjs
+cp build_qjs/libquickjs_c_bridge_plugin.so build/linux/x64/release/bundle/lib/
+
 cp -r build/linux/x64/release/bundle/* Solian.AppDir
 cp -r buildtools/appimage_config/* Solian.AppDir
 cp buildtools/icon-padded.png Solian.AppDir
