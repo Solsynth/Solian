bffa55c9fa7c376d2926ed69295713b8f6b4bc86
CNlongY-Py <CNlongY-Py@users.noreply.github.com>
2026-07-30 20:42:28 +0800

    sync: revert non-plugin non-notification files to Solsynth/Solian
    Keep plugin pane system and notification fixes; all other files
    restored to main repository state.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -36,8 +36,6 @@ subprojects {
     }
 }
 
-
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
--- lib/shared/widgets/app_startup_splash.dart
+++ lib/shared/widgets/app_startup_splash.dart
@@ -6,7 +6,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:island/accounts/account_pod.dart';
