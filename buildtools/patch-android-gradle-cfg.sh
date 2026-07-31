6063d20fa632cedce87ed8fae3f937b01a3346ed
LittleSheep <hi@littlesheep.me>
2026-07-31 20:39:35 +0800

    :bug: Patch android build

--- buildtools/patch-android-gradle-cfg.sh
+++ buildtools/patch-android-gradle-cfg.sh
@@ -4,28 +4,31 @@ set -e
 
 PUB_CACHE="${HOME}/.pub-cache/hosted/pub.dev"
 
+replace_in_file() {
+  local expression="$1"
+  local file="$2"
+
+  if [[ "$(uname)" == "Darwin" ]]; then
+    sed -i '' -E "$expression" "$file"
+  else
+    sed -i -E "$expression" "$file"
+  fi
+}
+
 echo "Patching Flutter plugin compileSdk versions to 36..."
 
 # Groovy DSL
 find "$PUB_CACHE" -type f -name "*.gradle" | while read -r file; do
-  sed -i '' \
-    -E 's/compileSdkVersion[[:space:]]+[0-9]+/compileSdkVersion 36/g' \
-    "$file"
+  replace_in_file 's/compileSdkVersion[[:space:]]+[0-9]+/compileSdkVersion 36/g' "$file"
 
-  sed -i '' \
-    -E 's/compileSdk[[:space:]]+[=:][[:space:]]*[0-9]+/compileSdk = 36/g' \
-    "$file"
+  replace_in_file 's/compileSdk[[:space:]]+[=:][[:space:]]*[0-9]+/compileSdk = 36/g' "$file"
 done
 
 # Kotlin DSL
 find "$PUB_CACHE" -type f -name "*.gradle.kts" | while read -r file; do
-  sed -i '' \
-    -E 's/compileSdk[[:space:]]*=[[:space:]]*[0-9]+/compileSdk = 36/g' \
-    "$file"
+  replace_in_file 's/compileSdk[[:space:]]*=[[:space:]]*[0-9]+/compileSdk = 36/g' "$file"
 
-  sed -i '' \
-    -E 's/compileSdkVersion\([[:space:]]*[0-9]+[[:space:]]*\)/compileSdkVersion(36)/g' \
-    "$file"
+  replace_in_file 's/compileSdkVersion\([[:space:]]*[0-9]+[[:space:]]*\)/compileSdkVersion(36)/g' "$file"
 done
 
 echo "Done."
